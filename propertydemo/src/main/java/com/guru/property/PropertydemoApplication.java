30ebfaad4f1e4edb8b259214b7d94a01dd2291cd
sallauddin023 <skallauddin023@gmail.com>
2020-09-29 12:28:17 +0530

    Initial Push

---
+++ propertydemo/src/main/java/com/guru/property/PropertydemoApplication.java
@@ -0,0 +1,13 @@
+package com.guru.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertydemoApplication.class, args);
+	}
+
+}
---
+++ propertydemo/src/test/java/com/guru/property/PropertydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
